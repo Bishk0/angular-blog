acf11e72b827eb9533a37dcfa12f149f22d4377b
Bishko <yurik322412@gmail.com>
2020-08-03 10:14:44 +0300

    Post creation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyAGGoYCkbGlUhr17V77YTuxYPtjsoOVWaY'
+  apiKey: 'AIzaSyAGGoYCkbGlUhr17V77YTuxYPtjsoOVWaY',
+  fbDbUrl: 'https://angular-blog-ae137.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyAGGoYCkbGlUhr17V77YTuxYPtjsoOVWaY'
+  apiKey: 'AIzaSyAGGoYCkbGlUhr17V77YTuxYPtjsoOVWaY',
+  fbDbUrl: 'https://angular-blog-ae137.firebaseio.com'
 };
 
 /*
